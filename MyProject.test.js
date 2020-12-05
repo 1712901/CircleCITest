f983f7aeb10df21c21cfcb6e84d3f91f2f6e0b57
= <tranvi9x@gmail.com>
2020-12-05 13:29:12 +0700

    Them test case

--- MyProject.test.js
+++ MyProject.test.js
@@ -1,7 +1,9 @@
 const sum = require('./MyProject');
 
-test('adds 1 + 2 to equal 3',sumTest);
+test('adds 1 + 2 to equal 3',sumTest(1,2));
+test('adds 2 + 2 to equal 4',sumTest(2,2));
+test('adds 2 - 2 to equal 0',sumTest(2,-2));
 
-function sumTest(){
-  expect(sum(1, 2)).toBe(3);
+function sumTest(a,b){
+  expect(sum(a,b)).toBe(a+b);
 };
